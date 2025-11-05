cd11168e5bb69d6cc921ef1b3d4c4d6aa9f7e056
FilbertSM <filbertsm7@gmail.com>
2025-11-05 21:47:39 +0700

    Improvement in responsive in more better pixel size

--- client/src/components/Creator.jsx
+++ client/src/components/Creator.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets"
 
 const Creator = () => {
     return(
-        <div id='creator' className='flex flex-col items-center gap-6 py-20 px-32 sm:px-48 lg:px-48 xl:px-48 text-center max-w-2xl mx-auto overflow-hidden'>
+        <div id='creator' className='flex flex-col items-center gap-6 py-20 px-24 sm:px-48 lg:px-48 xl:px-48 text-center max-w-2xl mx-auto overflow-hidden'>
             <a href="https://linkedin.com/in/filbert-sembiring-meliala/" className="group relative block bg-black rounded-2xl" target='_blank' rel='noopener noreferrer'>
             <img alt="" src={assets.hero_profile_img} className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-2xl" />
 
@@ -13,11 +13,11 @@ const Creator = () => {
                 <p className="text-xl font-bold text-white sm:text-2xl">Filbert SM</p>
 
                 <div className="mt-32 sm:mt-48 lg:mt-64">
-                <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-                    <p className="text-sm text-white">
-                    "If you're not prepared to be wrong, you'll never come up with anything original."<br />— Robert Greene
-                    </p>
-                </div>
+                    <div className="transform translate-y-0 opacity-100 transition-all sm:translate-y-8 sm:opacity-0 sm:group-hover:translate-y-0 sm:group-hover:opacity-100">
+                        <p className="text-sm text-white">
+                        "If you're not prepared to be wrong, you'll never come up with anything original."<br />— Robert Greene
+                        </p>
+                    </div>
                 </div>
             </div>
             </a>
