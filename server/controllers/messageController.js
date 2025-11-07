f886606d02fd5adb57c7d2368138f858055927b6
FilbertSM <filbertsm7@gmail.com>
2025-11-07 15:15:50 +0700

    Adding RAG system and fix z-index on sidebar with prismjs highlight

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -3,6 +3,7 @@ import Chat from "../models/Chat.js"
 import User from "../models/User.js"
 import imagekit from "../configs/imageKit.js"
 import openai from "../configs/openai.js"
+import personal from "../data/Personal.json" with { type: "json" }
 
 // Text-based AI Chat Message Controller
 export const textMessageController = async (req, res) => {
@@ -17,8 +18,63 @@ export const textMessageController = async (req, res) => {
         const chat = await Chat.findOne({userId, _id: chatId})
         chat.messages.push({role: "user", content: prompt, timestamp: Date.now(), isImage: false})
 
+        const systemPrompt = `
+            You are a **helpful, knowledgeable, and context-aware AI assistant**.  
+            You have access to reference data belonging to a person named **Filbert Sembiring Meliala**.
+
+            When the user asks any question related to **Filbert Sembiring Meliala**, you must:
+            - Use the provided data as your **primary source of truth**.  
+            - Respond **as if you personally know Filbert**, while remaining factual and concise.  
+            - Provide clear, accurate, and relevant information based on the data.  
+            - If the user asks **"Who is Filbert Sembiring Meliala?"**, generate a **short, well-structured profile summary** using the available details.  
+            - If the user asks something **unrelated to Filbert**, respond normally as a helpful assistant.
+
+            ---
+
+            ### Reference Data
+            ${JSON.stringify(personal, null, 2)}
+
+            User: ${prompt}
+        `;
+
         const { choices } = await openai.chat.completions.create({
-            model: "gemini-2.0-flash",
+            model: "gemini-2.5-flash",
+            messages: [
+//              { role: "system", content: "You are a helpful assistant."},
+                {
+                    role: "user",
+                    content: systemPrompt,
+                },
+            ],
+        });
+
+        const reply = {...choices[0].message, timestamp: Date.now(), isImage: false}
+        res.json({success: true, reply})
+
+        chat.messages.push(reply)
+        await chat.save()
+        await User.updateOne({_id: userId}, {$inc: {credits: - 1}})
+
+    } catch(error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Text-based AI Chat Message Controller
+export const textMessageRAGController = async (req, res) => {
+    try {
+        const userId = req.user._id
+        // Check Credit
+        if(req.user.credits < 1){
+            return res.json({success: false, message: "You don't have enough credits to use this feature. Have at least 1 credits to use it"})
+        }
+        const {chatId, prompt} = req.body
+
+        const chat = await Chat.findOne({userId, _id: chatId})
+        chat.messages.push({role: "user", content: prompt, timestamp: Date.now(), isImage: false})
+
+        const { choices } = await openai.chat.completions.create({
+            model: "gemini-2.5-flash",
             messages: [
 //              { role: "system", content: "You are a helpful assistant."},
                 {
